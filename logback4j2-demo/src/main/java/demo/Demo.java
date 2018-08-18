e7ecd860526f56554e6fa124c38bfcc4efd72171
William Luo <gluo7777@gmail.com>
2018-08-18 12:35:27 -0500

    logback examples

--- logback4j2-demo/src/main/java/demo/Demo.java
+++ logback4j2-demo/src/main/java/demo/Demo.java
@@ -1,14 +1,18 @@
 package demo;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Demo {
 	public static void main(String[] args) {
-		final Logger LOG = LoggerFactory.getLogger(Demo.class);
+		// This logger lets you use a java.util.Formatter string in the message to format parameters.
+		Logger LOG = LogManager.getFormatterLogger();
 		LOG.trace("Starting trace...");
 		doSomething1(LOG);
 		LOG.trace("Stopping trace...");
+		System.out.printf("Equal=%s\n",LOG.equals(LogManager.getFormatterLogger()));
+		LOG = LogManager.getRootLogger();
+		LOG.error("Logging at ROOT level"); // will not run since additivity=false
 	}
 
 	private static void doSomething1(final Logger LOG) {
